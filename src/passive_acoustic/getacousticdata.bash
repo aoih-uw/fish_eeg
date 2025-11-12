d1b8c247974cf6191b6295bfb2f47d6cd3ee90af
Michael James <mikejames.mj867@gmail.com>
2025-11-12 09:03:50 -0800

    for the bash scripts, fixed directory cd such that the script can be run from any pwd and the files will be put in the right relative dir. Created readme file for getting acoustic data.

--- src/passive_acoustic/getacousticdata.bash
+++ src/passive_acoustic/getacousticdata.bash
@@ -1,7 +1,19 @@
 echo "Make sure to run gsutilinit.bash first"
 sleep 2
 echo "Pulling Acoustic data..."
+
+# Get the directory where this script itself lives
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# cd to dir where script resides 
+cd $SCRIPT_DIR
+
+# cd back to fisheeg home
 cd ../..
+
+# make a data dir if needed
+[ -d "data" ] || mkdir -p "data"
+
 cd data
 echo "Moving to separate data folder"
 mkdir -p PAcoustic
